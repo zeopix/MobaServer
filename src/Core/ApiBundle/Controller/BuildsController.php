e4bacb9d73d93f10f0cc3fe548222d57b7225495
Iván Guillén <zeopix@gmail.com>
2013-04-14 22:58:05 +0200

    paging

--- src/Core/ApiBundle/Controller/BuildsController.php
+++ src/Core/ApiBundle/Controller/BuildsController.php
@@ -22,8 +22,10 @@ class BuildsController extends AbstractController
     	if(!$hero){
     		die("no resposne");
     	}
-    	$limit = 10;
-    	$builds = $this->getEm()->createQuery('SELECT b FROM CoreDataBundle:Build b WHERE b.hero = :hero and b.title is not null')->setParameter('hero',$hero)->setMaxResults($limit)->getResult();
+        $request = $this->getRequest();
+    	$limit = $request->get("limit",10);
+        $first=$request->get("start",0);
+    	$builds = $this->getEm()->createQuery('SELECT b FROM CoreDataBundle:Build b WHERE b.hero = :hero and b.title is not null')->setParameter('hero',$hero)->setFirstResult($first)->setMaxResults($limit)->getResult();
     	
     	$serialized = $this->getSerializer()->serialize($builds, 'json');
         return $this->createApiResponse($serialized);
